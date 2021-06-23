cd6d33c8e818a718601e45805cffa54ae4948aa7
Liz Wilson <lizriderwilson@gmail.com>
2021-06-23 13:00:51 -0400

    commit tree efb66521f20a35676c689ec55b0afc0effc138f4

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function submitData(userName, userEmail) {
+
+    let formData = {
+    name: userName,
+    email: userEmail
+    };
+
+    let configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+    };
+
+    return fetch('http://localhost:3000/users', configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            appendID(object);
+        })
+        .catch(function(error) {
+        document.body.innerHTML = "Unauthorized Access";
+  }); 
+}
+
+function appendID(userObj) {
+    //let div = document.createElement('div');
+    //div.innerText = userObj["id"];
+    //document.querySelector('body').appendChild(div);
+    document.body.innerHTML = userObj.id;
+}
